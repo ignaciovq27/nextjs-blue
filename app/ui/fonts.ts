a58a972021ecd15886521d581b789523912640d7
IgnacioVQ <ignacio.varasq@gmail.com>
2023-12-27 16:53:57 -0300

    first

---
+++ app/ui/fonts.ts
@@ -0,0 +1,20 @@
+import { Inter } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+import { Fredoka } from 'next/font/google';
+
+
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+    subsets: ['latin'],
+    weight: ['400', '700'],
+});
+
+export const fredoka = Fredoka({
+    subsets: ['latin'],
+    weight: ['400', '700'],
+});
+
+
+
